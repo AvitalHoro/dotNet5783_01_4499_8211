7f0a73b867b8ecb527993fff30216d5f7dc59fd9
רעות צפלביץ <tzipilev@g.jct.ac.il>
2022-10-31 15:17:58 +0200

    intial code+ Welcome function

---
+++ doteNet5783_01_4499_8211/Stage0/Program4499.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+         static void Main(string[] args)
+        {
+            Welcome4499();
+            Welcome8211();
+            Console.ReadKey();
+        }
+
+        private static void Welcome4499()
+        {
+            string s;
+            Console.WriteLine("Enter your name: ");
+            s = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", s);
+        }
+        static partial void Welcome8211();
+    }
+}
+
