d6b691f2babd4517d9765be4a91aad54e7670e74
רעות צפלביץ <tzipilev@g.jct.ac.il>
2022-11-09 11:42:20 +0200

    jj

---
+++ doteNet5783_01_4499_8211/DalFacade/DO/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace DalFacade.DO;
+
+public struct Order
+{
+ //   public override string ToString() => $@"
+	//Product ID={ID}: {Name}, 
+	//category - {Category}
+ //   	Price: {Price}
+ //   	Amount in stock: {InStock}
+	//";
+
+    public int ID { set; get; }
+    public string CostumerName { set; get; }
+    public string CostumerEmail { set; get; }
+    public string CostumerAdress { set; get; }
+    public DateTime OrderDate { set; get; }
+    public DateTime ShipDate { set; get; }
+    public DateTime DeliveryDate { set; get; }
+}
