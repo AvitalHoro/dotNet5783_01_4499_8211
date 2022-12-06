81e48802b722e6715bd6d1773bbb2fd3a0158dda
רעות צפלביץ <tzipilev@g.jct.ac.il>
2022-12-06 12:54:05 +0200

    tools

--- doteNet5783_01_4499_8211/BL/BO/Tools.cs
+++ doteNet5783_01_4499_8211/BL/BO/Tools.cs
@@ -1,12 +1,4 @@
 ﻿using DO;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BO;
 
