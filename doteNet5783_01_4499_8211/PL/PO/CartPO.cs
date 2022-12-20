8186507616acdf6088d632bb9f98b34878561b10
אביטל כהנא <avkahana@g.jct.ac.il>
2022-12-20 17:21:44 +0200

    PO

---
+++ doteNet5783_01_4499_8211/PL/PO/CartPO.cs
@@ -0,0 +1,91 @@
+﻿using BO;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace PO;
+
+class CartPO : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private string? costumerName;
+    public string? CostumerName
+    {
+        get
+        { return costumerName; }
+        set
+        {
+            costumerName = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("CostumerName"));
+            }
+        }
+    }
+
+
+    private string? costumerEmail;
+    public string? CostumerEmail
+    {
+        get
+        { return costumerEmail; }
+        set
+        {
+            costumerEmail = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("CostumerEmail"));
+            }
+        }
+    }
+
+
+    private string? costumerAdress;
+    public string? CostumerAdress
+    {
+        get
+        { return costumerAdress; }
+        set
+        {
+            costumerAdress = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("CostumerAdress"));
+            }
+        }
+    }
+
+
+    private double totalPrice;
+    public double TotalPrice
+    {
+        get
+        { return totalPrice; }
+        set
+        {
+            totalPrice = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("TotalPrice"));
+            }
+        }
+    }
+
+    private List<OrderItem?>? orderItems;
+    public List<OrderItem?>? OrderItems
+    {
+        get
+        { return orderItems; }
+        set
+        {
+            orderItems = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("OrderItems"));
+            }
+        }
+    }
+
+}
---
+++ doteNet5783_01_4499_8211/PL/PO/ProductPO.cs
@@ -0,0 +1,119 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace PO;
+
+class ProductPO : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private int id;
+    public int ID
+    {
+        get
+        { return id; }
+        set
+        {
+            id = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("ID"));
+            }
+        }
+    }
+
+    private string? name;
+    public string? Name
+    {
+        get
+        { return name; }
+        set
+        {
+            name = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+    }
+
+    private Category category;
+    public Category Category
+    {
+        get
+        { return category; }
+        set
+        {
+            category = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("Category"));
+            }
+        }
+    }
+
+    private double price;
+    public double Price
+    {
+        get
+        { return price; }
+        set
+        {
+            price = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("Price"));
+            }
+        }
+    }
+
+    private int inStock;
+    public int InStock
+    {
+        get
+        { return inStock; }
+        set
+        {
+            inStock = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("InStock"));
+            }
+        }
+    }
+
+    private bool isDeleted;
+    public bool IsDeleted
+    {
+        get
+        { return isDeleted; }
+        set
+        {
+            isDeleted = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("IsDeleted"));
+            }
+        }
+    }
+
+    private string? path;
+    public string? Path
+    {
+        get
+        { return path; }
+        set
+        {
+            path = value;
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("Path"));
+            }
+        }
+    }
+
+}
+
+ 
