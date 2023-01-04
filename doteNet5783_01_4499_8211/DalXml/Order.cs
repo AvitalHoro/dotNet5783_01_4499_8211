2e0fe61dc58ff25c187cd8f72c3735617d595f35
רעות צפלביץ <tzipilev@g.jct.ac.il>
2023-01-04 18:23:43 +0200

    PL products

--- doteNet5783_01_4499_8211/DalXml/Order.cs
+++ doteNet5783_01_4499_8211/DalXml/Order.cs
@@ -5,5 +5,47 @@ using System.Security.Principal;
 
 internal class Order : IOrder
 {
+    const string s_Orders = "Orders"; //XML Serializer
 
+    public IEnumerable<DO.Order?> GetAll(Func<DO.Order?, bool>? filter = null)
+    {
+        var listOrders = XMLTools.LoadListFromXMLSerializer<DO.Order>(s_Orders)!;
+        return filter == null ? listOrders.OrderBy(lec => ((DO.Order)lec!).ID)
+                              : listOrders.Where(filter).OrderBy(lec => ((DO.Order)lec!).ID);
+    }
+
+    public DO.Order GetById(int id) =>
+        XMLTools.LoadListFromXMLSerializer<DO.Order>(s_Orders).FirstOrDefault(p => p?.ID == id)
+        //DalMissingIdException(id, "Order");
+        ?? throw new Exception("missing id");
+
+    public int Add(DO.Order Order)
+    {
+        var listOrders = XMLTools.LoadListFromXMLSerializer<DO.Order>(s_Orders);
+
+        if (listOrders.Exists(lec => lec?.ID == Order.ID))
+            throw new Exception("id already exist");//DalAlreadyExistIdException(Order.ID, "Order");
+
+        listOrders.Add(Order);
+
+        XMLTools.SaveListToXMLSerializer(listOrders, s_Orders);
+
+        return Order.ID;
+    }
+
+    public void Delete(int id)
+    {
+        var listOrders = XMLTools.LoadListFromXMLSerializer<DO.Order>(s_Orders);
+
+        if (listOrders.RemoveAll(p => p?.ID == id) == 0)
+            throw new Exception("missing id"); //new DalMissingIdException(id, "Order");
+
+        XMLTools.SaveListToXMLSerializer(listOrders, s_Orders);
+    }
+
+    public void Update(DO.Order Order)
+    {
+        Delete(Order.ID);
+        Add(Order);
+    }
 }
--- doteNet5783_01_4499_8211/DalXml/Product.cs
+++ doteNet5783_01_4499_8211/DalXml/Product.cs
@@ -2,8 +2,80 @@
 using DalApi;
 using DO;
 using System.Security.Principal;
+using System.Xml.Linq;
 
 internal class Product : IProduct
 {
+    const string s_students = "students"; //Linq to XML
 
+    static DO.Product? getProduct(XElement p) =>
+        p.ToIntNullable("ID") is null ? null : new DO.Product()
+        {
+            ID = (int)p.Element("ID")!,
+            Name = (string?)p.Element("Name"),
+            Category = p.ToEnumNullable<DO.Category>("Category"),
+            Price = p.ToDoubleNullable("Price"),
+            InStock = p.ToIntNullable("InStock"),
+            IsDeleted = (bool)p.Element("IsDeleted"),
+            Path = p.(string?)p.Element("Path"),
+        };
+
+    static IEnumerable<XElement> createStudentElement(DO.Product student)
+    {
+        yield return new XElement("ID", student.ID);
+        if (student.FirstName is not null)
+            yield return new XElement("FirstName", student.FirstName);
+        if (student.LastName is not null)
+            yield return new XElement("LastName", student.LastName);
+        if (student.StudentStatus is not null)
+            yield return new XElement("StudentStatus", student.StudentStatus);
+        if (student.BirthDate is not null)
+            yield return new XElement("BirthDate", student.BirthDate);
+        if (student.LastName is not null)
+            yield return new XElement("Grade", student.Grade);
+    }
+
+    public IEnumerable<DO.Product?> GetAll(Func<DO.Product?, bool>? filter = null) =>
+        filter is null
+        ? XMLTools.LoadListFromXMLElement(s_students).Elements().Select(s => getStudent(s))
+        : XMLTools.LoadListFromXMLElement(s_students).Elements().Select(s => getStudent(s)).Where(filter);
+
+    public DO.Product GetById(int id) =>
+        (DO.Product)getStudent(XMLTools.LoadListFromXMLElement(s_students)?.Elements()
+        .FirstOrDefault(st => st.ToIntNullable("ID") == id)
+        // fix to: throw new DalMissingIdException(id);
+        ?? throw new Exception("missing id"))!;
+
+    public int Add(DO.Product student)
+    {
+        XElement studentsRootElem = XMLTools.LoadListFromXMLElement(s_students);
+
+        if (XMLTools.LoadListFromXMLElement(s_students)?.Elements()
+            .FirstOrDefault(st => st.ToIntNullable("ID") == student.ID) is not null)
+            // fix to: throw new DalMissingIdException(id);;
+            throw new Exception("id already exist");
+
+        studentsRootElem.Add(new XElement("Student", createStudentElement(student)));
+        XMLTools.SaveListToXMLElement(studentsRootElem, s_students);
+
+        return student.ID; ;
+    }
+
+    public void Delete(int id)
+    {
+        XElement studentsRootElem = XMLTools.LoadListFromXMLElement(s_students);
+
+        (studentsRootElem.Elements()
+            // fix to: throw new DalMissingIdException(id);
+            .FirstOrDefault(st => (int?)st.Element("ID") == id) ?? throw new Exception("missing id"))
+            .Remove();
+
+        XMLTools.SaveListToXMLElement(studentsRootElem, s_students);
+    }
+
+    public void Update(DO.Product doStudent)
+    {
+        Delete(doStudent.ID);
+        Add(doStudent);
+    }
 }
