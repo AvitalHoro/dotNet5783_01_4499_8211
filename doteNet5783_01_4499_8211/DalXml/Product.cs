acf98c5e18f3610899859f5f7f8453041482727f
רעות צפלביץ <tzipilev@g.jct.ac.il>
2023-01-08 00:00:45 +0200

    PL

--- doteNet5783_01_4499_8211/DalXml/Product.cs
+++ doteNet5783_01_4499_8211/DalXml/Product.cs
@@ -14,34 +14,31 @@ internal class Product : IProduct
             ID = (int)p.Element("ID")!,
             Name = (string?)p.Element("Name"),
             Category = p.ToEnumNullable<DO.Category>("Category"),
-            Price = p.ToDoubleNullable("Price"),
-            InStock = p.ToIntNullable("InStock"),
+            Price = (Double)p.Element("Price"),
+            InStock = (int)p.Element("InStock"),
             IsDeleted = (bool)p.Element("IsDeleted"),
-            Path = p.(string?)p.Element("Path"),
+            Path = (string?)p.Element("Path"),
         };
 
     static IEnumerable<XElement> createStudentElement(DO.Product student)
     {
         yield return new XElement("ID", student.ID);
-        if (student.FirstName is not null)
-            yield return new XElement("FirstName", student.FirstName);
-        if (student.LastName is not null)
-            yield return new XElement("LastName", student.LastName);
-        if (student.StudentStatus is not null)
-            yield return new XElement("StudentStatus", student.StudentStatus);
-        if (student.BirthDate is not null)
-            yield return new XElement("BirthDate", student.BirthDate);
-        if (student.LastName is not null)
-            yield return new XElement("Grade", student.Grade);
+        if (student.Name is not null)
+            yield return new XElement("LastName", student.Name);
+         yield return new XElement("StudentStatus", student.Category);
+         yield return new XElement("BirthDate", student.Price);
+         yield return new XElement("Grade", student.IsDeleted);
+        if (student.Path is not null)
+            yield return new XElement("Grade", student.Path);
     }
 
     public IEnumerable<DO.Product?> GetAll(Func<DO.Product?, bool>? filter = null) =>
         filter is null
-        ? XMLTools.LoadListFromXMLElement(s_students).Elements().Select(s => getStudent(s))
-        : XMLTools.LoadListFromXMLElement(s_students).Elements().Select(s => getStudent(s)).Where(filter);
+        ? XMLTools.LoadListFromXMLElement(s_students).Elements().Select(s => getProduct(s))
+        : XMLTools.LoadListFromXMLElement(s_students).Elements().Select(s => getProduct(s)).Where(filter);
 
     public DO.Product GetById(int id) =>
-        (DO.Product)getStudent(XMLTools.LoadListFromXMLElement(s_students)?.Elements()
+        (DO.Product)getProduct(XMLTools.LoadListFromXMLElement(s_students)?.Elements()
         .FirstOrDefault(st => st.ToIntNullable("ID") == id)
         // fix to: throw new DalMissingIdException(id);
         ?? throw new Exception("missing id"))!;
