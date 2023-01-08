4d4a8393533395145cabda6322dd49012687895c
אביטל כהנא <avkahana@g.jct.ac.il>
2023-01-08 17:16:56 +0200

    product xml

--- doteNet5783_01_4499_8211/DalXml/XMLTools.cs
+++ doteNet5783_01_4499_8211/DalXml/XMLTools.cs
@@ -18,16 +18,16 @@ static class XMLTools
 
     #region Extension Fuctions
     public static T? ToEnumNullable<T>(this XElement element, string name) where T : struct, Enum =>
-        Enum.TryParse<T>((string?)element.Element(name), out var result) ? (T?)result : null;
+        Enum.TryParse<T>((string)element.Element(name), out var result) ? (T)result : default;
 
     public static DateTime? ToDateTimeNullable(this XElement element, string name) =>
-        DateTime.TryParse((string?)element.Element(name), out var result) ? (DateTime?)result : null;
+        DateTime.TryParse((string)element.Element(name), out var result) ? (DateTime?)result : null;
 
     public static double? ToDoubleNullable(this XElement element, string name) =>
-        double.TryParse((string?)element.Element(name), out var result) ? (double?)result : null;
+        double.TryParse((string)element.Element(name), out var result) ? (double)result : default;
 
     public static int? ToIntNullable(this XElement element, string name) =>
-        int.TryParse((string?)element.Element(name), out var result) ? (int?)result : null;
+        int.TryParse((string)element.Element(name), out var result) ? (int)result : default;
     #endregion
 
     #region SaveLoadWithXElement
