2a229ad1bfa63776bffe23ccfe3810f0c513a708
רעות צפלביץ <tzipilev@g.jct.ac.il>
2023-01-11 21:33:52 +0200

    PL

--- doteNet5783_01_4499_8211/DalXml/DalXml.cs
+++ doteNet5783_01_4499_8211/DalXml/DalXml.cs
@@ -1,9 +1,12 @@
 ﻿namespace Dal;
 using DalApi;
 using DO;
+using System.Diagnostics;
 
 sealed class DalXml : IDal
 {
+    public static IDal Instance { get; } = new DalXml();
+    private DalXml() { }
     public IProduct Product { get; } = new Dal.Product();
     public IOrder Order { get; } = new Dal.Order();
     public IOrderItem OrderItem { get; } = new Dal.OrderItem();
