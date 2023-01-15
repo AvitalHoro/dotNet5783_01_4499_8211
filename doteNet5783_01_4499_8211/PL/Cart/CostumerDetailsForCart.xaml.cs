f9c39bd25e9c031b9628afc52fcd29a7fda06b75
רעות צפלביץ <tzipilev@g.jct.ac.il>
2023-01-15 09:21:13 +0200

    Products xml

---
+++ doteNet5783_01_4499_8211/PL/Cart/CostumerDetailsForCart.xaml.cs
@@ -0,0 +1,42 @@
+﻿using BLApi;
+using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.Cart;
+/// <summary>
+/// Interaction logic for CostumerDetailsForCart.xaml
+/// </summary>
+public partial class CostumerDetailsForCart : Window
+{
+    IBl bl;
+    BO.Cart cart;
+    public CostumerDetailsForCart(BO.Cart cart, IBl BL)
+    {
+        InitializeComponent();
+        this.cart = cart;
+        this.bl = BL;
+    }
+
+    private void approve_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+        cart.CostumerName = AddName.Text;
+        cart.CostumerEmail = AddEmail.Text;
+        cart.CostumerAdress = AddAddress.Text;
+
+        //MainWindow.framePage.Content = new Cart(bl, cart);
+        //new MainWindow(cart, bl).showCart();
+    }
+}
